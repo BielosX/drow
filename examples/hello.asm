f1a43c20a9fa48903ffa0e71450ee3f0acc97afe
Mateusz Biel <matbiel@gmail.com>
2022-02-16 21:47:44 +0100

    Use NASM instead of Gnu Assembler

---
+++ examples/hello.asm
@@ -0,0 +1,17 @@
+section .text
+    global _start
+
+_start:
+    mov rax, 1
+    mov rdi, 1
+    lea rsi, [rel msg] ; lea rsi,[rip + (msg - nextInstruction)]
+    mov rdx, msg_len
+    syscall
+    
+    mov rax, 60
+    mov rdi, 0
+    syscall
+
+section .rodata
+    msg: db 'Hello World!'
+    msg_len: equ $ - msg
